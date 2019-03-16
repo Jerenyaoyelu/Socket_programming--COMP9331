6be694a7304cd7a363a7877d3f37666a577979c9
YAOYELU <luyaoyejeren@gmail.com>
2019-03-16 19:53:33 +1100

    [rewrite]Change the idea about send what content : think simply---just the header and the content of the required file

--- WebServer.py
+++ WebServer.py
@@ -1,34 +1,34 @@
 import sys
 import socket
+from os import curdir,sep
 
 #invalid request exception
 class InvalidRequest(Exception):
     pass
 
-#process raw request
-class Request():
-    def __init__(self,raw_request):
-        self.raw_request = raw_request
-        self.method, self.path, self.protocol, self.header = self.parse_request()
+# #process raw request
+# class Request():
+#     def __init__(self,raw_request):
+#         self.raw_request = raw_request
+#         self.method, self.path, self.protocol, self.header = self.parse_request()
 
-    def parse_request(self):
-        #encode the request
-        unpack = [line.strip() for line in self.raw_request.splitlines()]
+#     def parse_request(self):
+#         #encode the request
+#         unpack = [line.strip() for line in self.raw_request.splitlines()]
 
-        #check the validation of request method
-        method,path,protocol = [k.strip() for k in unpack[0].split()]
-        if method.decode() != 'GET':
-            raise InvalidRequest("Only accepts GET requests")
+#         #check the validation of request method
+#         method,path,protocol = [k.strip() for k in unpack[0].split()]
+#         if method.decode() != 'GET':
+#             raise InvalidRequest("Only accepts GET requests")
         
-        #create the header
-        header = {}
-        for t in unpack[1:-1]:
-            for h,c in t.strip().split(":",1):
-                header[h.strip()] = c.strip()
-        return method, path, protocol, header
+#         # create the header
+#         header = {}
+#         for k, v in [i.split(':', 1) for i in unpack[1:-1]]:
+#             header[k.strip()] = v.strip()
+#         return method, path, protocol, header
 
-    def __repr__(self):
-        return repr({'method':self.method,'path':self.path,'protocol':self.protocol,'header':self.header})
+#     def __repr__(self):
+#         return repr({'method': self.method, 'path': self.path, 'protocol': self.protocol, 'headers': self.header})
         
 #Set up the server
 #Specify the port 
@@ -46,10 +46,28 @@ while True:
     clientsock,addr = serversock.accept()
     try:
         message = clientsock.recv(1024)
-        request = Request(message)
-        print(repr(request))
-        clientsock.send(repr(request))
-        clientsock.close()
+
+        #check the validation of request method
+        method = message.split()[0].strip()
+        print(method)
+        if method.decode() != 'GET':
+            raise InvalidRequest("Only accepts GET requests")
+        
+        #read the request
+        path = message.split()[1].strip()
+        filename = path.decode()[1:]
+        try:
+            with open (filename,'rb') as f:
+                files= f.read()
+            # print(files)
+            length = len(files)
+            #create response
+            # h = "HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Length: {}\r\n\r\n".format(length)
+            # response = h+files
+            # clientsock.sendall(bytes(response.encode("utf-8")))
+            clientsock.close()
+        except IOError:
+            clientsock.send(bytes('HTTP/1.1 400 Not Found\r\n','utf-8'))
     except InvalidRequest as e:
         clientsock.send(bytes('HTTP/1.1 400 Not Found\r\n','utf-8'))
         clientsock.send('Content-Type: text/html' + '\r\n'*2)
