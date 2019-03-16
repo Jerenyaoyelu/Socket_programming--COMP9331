404b88c28556477ae2d96ffa58eb92f82d5bb144
YAOYELU <luyaoyejeren@gmail.com>
2019-03-16 17:08:38 +1100

    [restructure] rewrite the structure of code

--- WebServer.py
+++ WebServer.py
@@ -1,31 +1,36 @@
-# from http.server import HTTPServer, BaseHTTPRequestHandler
-# from os import curdir
-# import sys
-
-# if len(sys.argv)!=2:
-#     print("No Such Command!")
-#     print("Usage:$python Filename.py port")
-#     sys.exit()
-# class Serv(BaseHTTPRequestHandler):
-#     def do_GET(self):
-#         try:
-#             file_to_open = open(curdir+self.path)
-#             self.send_response(200)
-#             self.wfile.write(bytes(file_to_open.read(),'utf-8'))
-#         except:
-            
-#             self.send_response(404)
-#         self.end_headers()
-            
-
-# httpd = HTTPServer(('localhost',int(sys.argv[1])),Serv)
-# httpd.serve_forever()
-
-
 import sys
 import socket
-from os import curdir
 
+#invalid request exception
+class InvalidRequest(Exception):
+    pass
+
+#process raw request
+class Request():
+    def __init__(self,raw_request):
+        self.raw_request = raw_request
+        self.method, self.path, self.protocol, self.header = self.parse_request()
+
+    def parse_request(self):
+        #encode the request
+        unpack = [line.strip() for line in self.raw_request.splitlines()]
+
+        #check the validation of request method
+        method,path,protocol = [k.strip() for k in unpack[0].split()]
+        if method.decode() != 'GET':
+            raise InvalidRequest("Only accepts GET requests")
+        
+        #create the header
+        header = {}
+        for t in unpack[1:-1]:
+            for h,c in t.strip().split(":",1):
+                header[h.strip()] = c.strip()
+        return method, path, protocol, header
+
+    def __repr__(self):
+        return repr({'method':self.method,'path':self.path,'protocol':self.protocol,'header':self.header})
+        
+#Set up the server
 #Specify the port 
 if len(sys.argv)!=2:
     print("No Such Command!")
@@ -34,26 +39,18 @@ if len(sys.argv)!=2:
 port= int(sys.argv[1])
 serversock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 serversock.bind(('',port))
-serversock.listen(1) #listen for connection
-# print("Web server up on port",port) #print port address
+serversock.listen(5) #listen for connection
 
 #Start the while loop.
 while True:
     clientsock,addr = serversock.accept()
     try:
         message = clientsock.recv(1024)
-        path = message.split()[1] # message.split()[1]is bytes type, can be decoded by using decode()
-        # print(path)
-        # p2 = path.decode()
-        # print(curdir+p2[1:])
-        # f = open(curdir+p2[1:])
-        # clientsock.send(bytes('HTTP/1.1 200 OK \r\n','utf-8'))
-        # clientsock.send(bytes(f.read(),'utf-8'))
+        request = Request(message)
+        print(repr(request))
+        clientsock.send(repr(request))
         clientsock.close()
-
-
-    except IOError:
-        pass
-        #Send response message for the file not found.
-        print ("404 Not Found")
-        clientsock.send(bytes('HTTP/1.1 404 Not Found \r\n','utf-8'))
+    except InvalidRequest as e:
+        clientsock.send(bytes('HTTP/1.1 400 Not Found\r\n','utf-8'))
+        clientsock.send('Content-Type: text/html' + '\r\n'*2)
+        clientsock.send('<h1>Invalid Request: %s</h1>' % e)
